5b900bfcc28e8110b3b70d3868cb41497662aa03
ViktorLukashchuk <78234389+ViktorLukashchuk@users.noreply.github.com>
2023-06-08 22:36:17 +0300

    Add files via upload

---
+++ hw1/main.js
@@ -0,0 +1,36 @@
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let aa = 10;
+let bb = -999;
+let cc = 123;
+let dd = 123;
+let ff = 3.14;
+let ee = 2.7;
+let ab = 16;
+let ba = true;
+let bab = false;
+
+console.log(a, b, c, d, e, aa, bb, cc, dd, ff, ee, ab, ba, bab);
+
+const firstName = 'Viktor';
+const middleName = 'Anatoliyovych';
+const lastName = 'Lukashchuk';
+
+let person = `${firstName} ${middleName} ${lastName}`
+console.log('my full name is: ' + person);
+
+let aaa = 100; let bbb = '100'; let ccc = true;
+
+console.log('Type of aaa is ' + typeof(aaa));
+console.log('Type of bbb is ' + typeof(bbb));
+console.log('Type of ccc is ' + typeof(ccc));
+
+let fName = prompt('what is your first name?');
+let lName = prompt('what is your last name?');
+let age = prompt('what is your age?');
+
+const fullName = `Your full name is ${fName} ${lName} and you are ${age} old`
+console.log(fullName);
